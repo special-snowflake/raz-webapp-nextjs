9ef61b233d63608f9b07d0f7351be501cefbc37c
achmad anshori <utd.rik@gmail.com>
2022-02-16 04:19:00 +0700

    add page notif&forgot, fix global styling

---
+++ src/pages/auth/forgot-password.js
@@ -0,0 +1,34 @@
+import styles from "src/common/styles/Forgot.module.css";
+
+import PageTitle from "src/common/components/PageTitle";
+
+function Forgot(props) {
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const body = {
+      email: e.target.email.value,
+    };
+    console.log(body);
+  };
+
+  return (
+    <>
+      <PageTitle
+        title="Forgot Password"
+        subTitle="Forgot your password? don't worry, we got your back."
+      />
+      <main className={styles["main"]}>
+        <form onSubmit={submitHandler} className={styles["form"]}>
+          <input
+            name="email"
+            placeholder="Your email address *"
+            className={styles["email"]}
+          ></input>
+          <button>Reset Password</button>
+        </form>
+      </main>
+    </>
+  );
+}
+
+export default Forgot;
---
+++ src/pages/notification/index.js
@@ -0,0 +1,59 @@
+import styles from "src/common/styles/Notification.module.css";
+
+import PageTitle from "src/common/components/PageTitle";
+
+function Notification(props) {
+  return (
+    <>
+      <PageTitle
+        title="Notification"
+        subTitle="See your notifications for the latest updates"
+      />
+      <main className={styles["main"]}>
+        <div className={styles["notification-list"]}>
+          <div className={styles["notification-item"]}>
+            <div className={styles["notification-dot"]}></div>
+            <p
+              className={`${styles["notification-title"]} ${styles["unread"]}`}
+            >
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </p>
+            <p className={styles["notification-content"]}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit
+              dapibus non elementum arcu tortor...
+            </p>
+          </div>
+          <div className={styles["notification-item"]}>
+            <p className={styles["notification-title"]}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </p>
+            <p className={styles["notification-content"]}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit
+              dapibus non elementum arcu tortor...
+            </p>
+          </div>
+          <div className={styles["notification-item"]}>
+            <p className={styles["notification-title"]}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </p>
+            <p className={styles["notification-content"]}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit
+              dapibus non elementum arcu tortor...
+            </p>
+          </div>
+        </div>
+        <div className={styles["pagination-buttons"]}>
+          <button className={`${styles["button"]} ${styles["active"]}`}>
+            01
+          </button>
+          <button className={`${styles["button"]}`}>02</button>
+          <button className={`${styles["button"]}`}>03</button>
+          <button className={`${styles["button"]}`}>04</button>
+          <button className={`${styles["button"]}`}>05</button>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default Notification;
