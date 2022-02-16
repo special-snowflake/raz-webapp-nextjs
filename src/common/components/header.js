fdb3994af5211c73f8f4ed704b4828383079dfce
ikehikeh151 <ikehikeh151@gmail.com>
2022-02-16 16:13:44 +0700

    fix modal user after/before login

--- src/common/components/header.js
+++ src/common/components/header.js
@@ -3,7 +3,17 @@ import React, { useState, useEffect } from "react";
 import Link from "next/link";
 
 function Header() {
-  const [login, setLogin] = useState(true);
+  const [login, setLogin] = useState(false);
+  const [toggleAuth, setToggleAuth] = useState(false);
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  const toggleAuthSwitch = () => {
+    toggleAuth ? setToggleAuth(false) : setToggleAuth(true);
+  };
+
+  const toggleMenuSwitch = () => {
+    toggleMenu ? setToggleMenu(false) : setToggleMenu(true);
+  };
 
   return (
     <header className={styles["wrapper-header"]}>
@@ -12,6 +22,12 @@ function Header() {
         <li className={styles["list-menu"]}>HOME</li>
         <li className={styles["list-menu"]}>
           PAGES <i className="bi bi-caret-down-fill"></i>
+          <ul className={styles["wrapper-submenu"]}>
+            <li className={styles["list-submenu"]}>asdasd</li>
+            <li className={styles["list-submenu"]}>asdasd</li>
+            <li className={styles["list-submenu"]}>asdasd</li>
+            <li className={styles["list-submenu"]}>asdasd</li>
+          </ul>
         </li>
         <li className={styles["list-menu"]}>
           SHOP <i className="bi bi-caret-down-fill"></i>
@@ -22,7 +38,7 @@ function Header() {
         <i className={`bi bi-search ${styles.icon}`}></i>
         <i className={`bi bi-heart ${styles.icon}`}></i>
         <i className={`bi bi-cart ${styles.icon}`}></i>
-        <div className={styles["wrapper-btn-menu"]}>
+        <div className={styles["wrapper-btn-menu"]} onClick={toggleAuthSwitch}>
           <span></span>
           <span></span>
           <span></span>
@@ -30,9 +46,12 @@ function Header() {
       </div>
 
       {login ? (
-        <MenuLogin />
+        <MenuLogin show={toggleAuth} />
       ) : (
-        <ul className={styles["wrapper-menu"]}>
+        <ul
+          className={
+            !toggleAuth ? styles["wrapper-menu"] : styles["wrapper-menu-show"]
+          }>
           <li>Login</li>
           <li>Register</li>
           <li>Chat</li>
@@ -43,9 +62,10 @@ function Header() {
   );
 }
 
-function MenuLogin() {
+function MenuLogin({ show }) {
   return (
-    <ul className={styles["wrapper-menu"]}>
+    <ul
+      className={!show ? styles["wrapper-menu"] : styles["wrapper-menu-show"]}>
       <li>Profile</li>
       <li>Chat</li>
       <li>Notification</li>
