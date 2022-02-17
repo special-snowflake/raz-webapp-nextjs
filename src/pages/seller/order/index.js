d2b6feec262e5db94879bbd5eafd5ce62e1d89de
ikehikeh151 <ikehikeh151@gmail.com>
2022-02-17 13:52:50 +0700

    fix margin icon versi mobile, add fix routing seller

---
+++ src/pages/seller/order/index.js
@@ -0,0 +1,5 @@
+function Order() {
+  return <h1>Halaman Order Seller</h1>;
+}
+
+export default Order;
---
+++ src/pages/seller/product/index.js
@@ -0,0 +1,4 @@
+function Product() {
+  return <h1>Halaman Product Seller</h1>;
+}
+export default Product;
