a29a216b8d9c5250e6f981227443755682c69afd
special-snowflake <fzn475@gmail.com>
2022-02-20 21:32:38 +0700

    changing profile

---
+++ src/common/components/Logout.js
@@ -0,0 +1,51 @@
+import {useState} from 'react';
+import {Modal} from 'react-bootstrap';
+import modalsCss from 'src/common/styles/Modals.module.css';
+
+function Logout(props) {
+  const [isShow, setIsShow] = useState(props.isShow);
+  console.log(props);
+  const hanndleLogout = () => {
+    console.log('logout');
+  };
+  return (
+    <div>
+      <Modal show={isShow}>
+        <Modal.Header>
+          <p className={modalsCss['header']}>Logout</p>
+          <button
+            type='button'
+            className={`${modalsCss['close-btn']}`}
+            data-bs-dismiss='modal'
+            aria-label='close'
+            onClick={() => {
+              setModaleLogout({
+                show: false,
+              });
+            }}></button>
+        </Modal.Header>
+        <Modal.Body>
+          <p className='color-grey text-center font-normal'>
+            Are you sure you want to Logout?
+          </p>
+        </Modal.Body>
+        <Modal.Footer>
+          <button
+            className={`btn ${modalsCss['btn-black']} float-start`}
+            onClick={hanndleLogout}>
+            Logout
+          </button>
+          <button
+            onClick={() => {
+              setIsShow(false);
+            }}
+            className={`btn ${modalsCss['btn-gray']}`}>
+            Cancel
+          </button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
+}
+
+export default Logout;
--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -1,16 +1,26 @@
-import styles from "src/common/styles/Notification.module.css";
-import Header from "src/common/components/header";
-import PageTitle from "src/common/components/PageTitle";
-import Footer from "src/common/components/footer";
+import styles from 'src/common/styles/Notification.module.css';
+import Header from 'src/common/components/header';
+import PageTitle from 'src/common/components/PageTitle';
+import Footer from 'src/common/components/footer';
+import Logout from 'src/common/components/Logout';
+import ProfileSeller from 'src/common/components/ProfileSeller';
+import {useState} from 'react';
 
-function Prrofile(props) {
+function Profile(props) {
+  const [showLogout, setShowLogout] = useState(false);
   return (
     <>
       <Header />
-      <main className={styles["main"]}></main>
+      <PageTitle
+        title='Profile'
+        subTitle='See your notifications for the latest updates'
+      />
+      <main className={styles['main']}></main>
+      <ProfileSeller />
+      <Logout isShow={showLogout} />
       <Footer />
     </>
   );
 }
 
-export default Prrofile;
+export default Profile;
