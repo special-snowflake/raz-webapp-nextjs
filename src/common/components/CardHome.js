b3dd61e175264a5fb32f3fd932f4cd0094ed46c4
ikehikeh151 <ikehikeh151@gmail.com>
2022-02-20 16:34:11 +0700

    fix page home, footer,add component home, cardhome,delete config toast login/register, add img bangku

---
+++ src/common/components/CardHome.js
@@ -0,0 +1,37 @@
+import Link from "next/link";
+import Image from "next/image";
+import styles from "src/common/styles/Home.module.css";
+import defaultProduct from "public/defaultProduct.jpg";
+
+function CardHome({ id, name, price, image }) {
+  console.log("IMAGE", image);
+  const host = process.env.NEXT_PUBLIC_API_URL;
+  const imageCard = host + image;
+  console.log("IMAGE-CARD", imageCard);
+  return (
+    <>
+      <Link href={`/product/${id}`} passHref>
+        <div className={styles["card-wrapper"]}>
+          <div className={styles["img-wrapper"]}>
+            <Image
+              alt="product name"
+              src={defaultProduct}
+              layout="fill"
+              objectFit="cover"
+              priority={true}
+              onError={() => {
+                setImage(defaultProduct);
+              }}
+              // onClickHandler={() => onClickHandler}
+            />
+          </div>
+          <div className={styles["product-detail"]}>
+            <p className={styles["product-name"]}>{name}</p>
+            <p className={styles["product-price"]}>{price}</p>
+          </div>
+        </div>
+      </Link>
+    </>
+  );
+}
+export default CardHome;
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,9 @@ import Head from "next/head";
 import styles from "src/common/styles/Home.module.css";
 import Header from "src/common/components/header";
 import Footer from "src/common/components/footer";
-import PageTitle from "src/common/components/PageTitle";
+import Home from "src/common/components/Home";
 
-export default function Home() {
+export default function Index() {
   return (
     <div className={styles.container}>
       <Head>
@@ -12,46 +12,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <PageTitle title="HOME" subTitle="SUB JUDUL" />
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}>
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}>
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+      <Home />
 
       <Footer />
     </div>
