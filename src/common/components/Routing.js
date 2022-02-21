0aff70e194a62718dc20e35a0d83d35ff9c56de2
special-snowflake <fzn475@gmail.com>
2022-02-22 01:22:05 +0700

    fixing routing

--- src/common/components/Routing.js
+++ src/common/components/Routing.js
@@ -10,31 +10,29 @@ function Routing(props) {
   const router = useRouter();
   useEffect(() => {
     console.log('isi user', user);
-    if (props.type) {
-      if (props.type.toLowerCase() === 'public') {
-        if (user.token) {
-          return router.replace('/');
-        }
-      } else if (props.type.toLowerCase() === 'private') {
-        if (!user.token) {
-          toast.warning('Please login before accessing this page');
-          return router.replace('/auth');
-        } else {
-          if (props.user.toLowerCase() === 'all') {
-            if (!user.roles) {
-              toast.warning('Please login before accessing this page');
-              return router.replace('/');
-            }
-          } else if (props.user.toLowerCase() === 'customer') {
-            if (user.roles !== '2') {
-              toast.warning(`Only customer can access this page`);
-              return router.replace('/');
-            }
-          } else if (props.user.toLowerCase() === 'seller') {
-            if (user.roles !== '1') {
-              toast.warning(`Only seller can access this page`);
-              return router.replace('/');
-            }
+    if (props.type.toLowerCase() === 'public') {
+      if (user.token) {
+        return router.replace('/');
+      }
+    } else if (props.type.toLowerCase() === 'private') {
+      if (!user.token) {
+        toast.warning('Please login before accessing this page');
+        return router.replace('/auth');
+      } else {
+        if (props.user.toLowerCase() === 'all') {
+          if (!user.token) {
+            toast.warning('Please login before accessing this page');
+            return router.replace('/');
+          }
+        } else if (props.user.toLowerCase() === 'customer') {
+          if (user.roles !== '2') {
+            toast.warning(`Only customer can access this page`);
+            return router.replace('/');
+          }
+        } else if (props.user.toLowerCase() === 'seller') {
+          if (user.roles !== '1') {
+            toast.warning(`Only seller can access this page`);
+            return router.replace('/');
           }
         }
       }
